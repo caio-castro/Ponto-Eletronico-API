1c2c49d7ddeda9406d02924cacf5baea95a7dd3a
caio castro <caio-rb@hotmail.com>
2020-11-26 16:43:16 -0300

    Alteração feita no password devido a configuração do Spring Security

--- src/main/java/com/kazale/pontointeligente/api/utils/PasswordUtils.java
+++ src/main/java/com/kazale/pontointeligente/api/utils/PasswordUtils.java
@@ -2,7 +2,6 @@ package com.kazale.pontointeligente.api.utils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordUtils {
 
@@ -23,8 +22,9 @@ public class PasswordUtils {
 		}
 
 		log.info("Gerando hash com o BCrypt.");
-		BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
-		return bCryptEncoder.encode(senha);
+	//	BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+	//	return bCryptEncoder.encode(senha);
+		return senha;
 	}
 
 }
